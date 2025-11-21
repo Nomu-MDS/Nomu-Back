d02396556933c24ced81d4c57aaca0e4ce89ef8e
Abdel-Rahim <abdel-rahim.khababa@hotmail.com>
2025-11-21 09:44:21 +0100

    feat/creation models Message

--- app/models/index.js
+++ app/models/index.js
@@ -4,11 +4,13 @@ import UserModel from "./User.js";
 import ProfilModel from "./Profil.js";
 import InteretModel from "./Interet.js";
 import ConversationModel from "./Conversation.js";
+import MessageModel from "./Message.js";
 
 const User = UserModel(sequelize);
 const Profil = ProfilModel(sequelize);
 const Interet = InteretModel(sequelize);
 const Conversation = ConversationModel(sequelize);
+const Message = MessageModel(sequelize);
 
 // Relations
 User.hasOne(Profil, { foreignKey: "userId" });
@@ -23,4 +25,11 @@ User.hasMany(Conversation, { as: "ConversationsAsLocal", foreignKey: "localID" }
 Conversation.belongsTo(User, { as: "Voyager", foreignKey: "voyagerID" });
 Conversation.belongsTo(User, { as: "Local", foreignKey: "localID" });
 
-export { sequelize, User, Profil, Interet, Conversation };
+// Relations Message
+User.hasMany(Message, { foreignKey: "userID", as: "SentMessages" });
+Message.belongsTo(User, { foreignKey: "userID", as: "Sender" });
+
+Conversation.hasMany(Message, { foreignKey: "convID", as: "Messages" });
+Message.belongsTo(Conversation, { foreignKey: "convID", as: "Conversation" });
+
+export { sequelize, User, Profil, Interet, Conversation, Message };
